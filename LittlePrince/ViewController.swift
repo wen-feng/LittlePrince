59f4d7bf4312796d5d63b004596b9bd5ec1d053e
Wu, Weng-Feng <wuweng-feng@1505006nb01.local>
2016-03-20 17:15:39 +0800

    Initial Commit

---
+++ LittlePrince/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LittlePrince
+//
+//  Created by Wu, Weng-Feng on 2016/3/20.
+//  Copyright © 2016年 Wu, Weng-Feng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
